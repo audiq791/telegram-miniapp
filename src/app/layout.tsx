54fa6cbc45c6fcc1fce97180611a3e56afac84be
Levan <audiq791@gmail.com>
2026-02-17 01:05:57 +0300

    йвйца

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Manrope } from "next/font/google";
-import { useLargeScale } from "../hooks/useLargeScale";
 
 const manrope = Manrope({
   subsets: ["cyrillic", "latin"],
@@ -15,15 +14,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const isLargeScale = useLargeScale();
-
   return (
     <html lang="ru" className={manrope.variable}>
       <head>
         {/* Запрещаем масштабирование на iPhone */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
-      <body className={`font-sans ${isLargeScale ? 'large-scale' : ''}`}>
+      <body className="font-sans">
         {children}
       </body>
     </html>
